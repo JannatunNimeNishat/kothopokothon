89f0301ca73a8c9e9b91085593275d023794b579
JannatunNimeNishat <nishatnime100@gmail.com>
2023-04-23 23:32:24 +0600

    starter

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA13dJpD59DVbSCdjY6Zimgjk_1jnmKA_s",
+  authDomain: "kothopokothon-ccc89.firebaseapp.com",
+  projectId: "kothopokothon-ccc89",
+  storageBucket: "kothopokothon-ccc89.appspot.com",
+  messagingSenderId: "920683619405",
+  appId: "1:920683619405:web:7e043e5fd6f8edb5886804"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Chat from "../layouts/Chat";
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<Main></Main>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            },
+            {
+                path:'login',
+                element:<Login></Login>
+            },
+            {
+                path:'register',
+                element:<Register></Register>
+            }
+
+        ]
+    },
+    {
+        path:'chat',
+        element:<Chat></Chat> //this will be rapped with  protected route
+    }
+])
+
+export default router;
